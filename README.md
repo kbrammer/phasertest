39788b422dde4c5a1036dc369ad0f21b0a8ae142
Kevin Brammer <me@kevinbrammer.com>
2016-09-24 13:00:49 -0500

    remove unused files, add license, readme, gitignore

---
+++ README.md
@@ -0,0 +1,9 @@
+# Phaser Test
+
+Phaser IO sandbox
+
+[Example](https://kbrammer.github.io/phasertest/)
+
+## License 
+
+MIT
--- preload.js
+++
@@ -1,6 +0,0 @@
-function preload() {
-    this.forceSingleUpdate = true;
-    this.load.image('space_ship', 'space-ship.png');
-}
-
-export default preload;
